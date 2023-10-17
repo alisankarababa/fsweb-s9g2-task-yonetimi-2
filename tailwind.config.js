a91c6c21c5d0e53db23eb23f9e39dc6bf5a05ad5
Alişan Karababa <alisankarababa@gmail.com>
2023-10-17 13:44:45 +0300

    task classı ve taskaltındaki h3, p , ve buton cssleri tailwind ile güncellendi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        boxShadow: {
+            "task" : "0 4px 5px 0 rgb(0 0 0 / 10%)",
+            "task-btn" : "0 4px 5px 0 rgb(0 0 0 / 5%)",
+        }
+      },
     },
     plugins: [],
   }
