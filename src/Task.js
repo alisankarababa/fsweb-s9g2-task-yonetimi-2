5eaa0644e2a7418813e419bfeed6c3f091487485
Alişan Karababa <alisankarababa@gmail.com>
2023-10-17 12:11:06 +0300

    son teslim tarihi tarih formatından kalan gün formatına dönüştürüldü, arkaplan rengi deadline'a kalan zamana göre değiştirildi

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { formatDistanceToNow, differenceInDays } from 'date-fns'
+import { tr } from "date-fns/locale";
 
 const Task = ({ taskObj, onComplete }) => {
+
+
+    function getClassNameBgColor(date) {
+        const diffDay = differenceInDays(date , new Date());
+        return diffDay > 3 ? "normal" : "urgent";
+    }
+
+    const deadlineInDate = new Date(taskObj.deadline)
+
+    const formattedDeadline = formatDistanceToNow(deadlineInDate, {addSuffix:true, locale: tr});
+    const classBg = getClassNameBgColor(deadlineInDate);
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">son teslim: <span className={classBg}>{formattedDeadline}</span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
